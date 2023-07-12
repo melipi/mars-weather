056d723bc71fef24a5319591ee1e3012df196cf3
melipi <melissapita@gmail.com>
2023-07-11 21:36:49 -0400

    Beginning structure for components

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageLayout from './PageLayout';
+
+const App = () => {
+
+  return (
+    <PageLayout />
+  );
+};
+
+export default App;
---
+++ src/components/PageLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './PageLayout.css';
+
+const PageLayout = () => {
+
+    return (
+        <div className='background'></div>
+    );
+};
+
+export default PageLayout;
